ebd930482790eff63d00584914019d4ca8b1f8ba
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-06-01 16:49:48 -0300

    front quase finalizado

---
+++ abrirModal.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const abrirModal = ()=>{
+    const modal = document.getElementById('modal')
+    modal.classList.add('active')
+}
+
+const fecharModal = ()=>{
+    const modal = document.getElementById('modal')
+    modal.classList.remove('active')
+}
+
+document.getElementById('abrir-modal').addEventListener('click', abrirModal)
+document.getElementById('fechar-modal').addEventListener('click', fecharModal)
+document.getElementById('modal').addEventListener('click', fecharModal)
