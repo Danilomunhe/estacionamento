1e4f342f278600ecd1b6d43f6adcc90a7b060847
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-06-02 16:45:52 -0300

    front finalizado

--- abrirModal.js
+++ abrirModal.js
@@ -10,6 +10,14 @@ const fecharModal = ()=>{
     modal.classList.remove('active')
 }
 
+function abrir() {
+    document.querySelector(".menu-burger-container").classList.toggle("active");
+  }
+  function fechar() {
+    document.querySelector(".menu-burger-container").classList.remove("active");
+  }
+  
+document.querySelector(".menu-burger-container").addEventListener("click", abrir);
 document.getElementById('abrir-modal').addEventListener('click', abrirModal)
 document.getElementById('fechar-modal').addEventListener('click', fecharModal)
-document.getElementById('modal').addEventListener('click', fecharModal)
+// document.getElementById('modal').addEventListener('click', fecharModal)
